fd04fcaf3725b382eef06df16c0f4f64ff6d417f
Ira Horecka <ira89@icloud.com>
2021-11-19 13:27:27 -0500

    build working api file uploader for CSV (or anything...)

--- llrws/api/endpoints.py
+++ llrws/api/endpoints.py
@@ -1,6 +1,19 @@
-from flask_restful import Resource
+from uuid import uuid4
+
+from flask_restful import Resource, reqparse
+import werkzeug
 
 
 class HelloWorld(Resource):
     def get(self):
         return {"hello": "world"}
+
+    def post(self):
+        # Example cURL call: curl -v POST -H "Content-Type: multipart/form-data" -F "file=@table-1.csv" http://api.localhost:5000
+        parse = reqparse.RequestParser()
+        parse.add_argument("file", type=werkzeug.datastructures.FileStorage, location="files")
+        args = parse.parse_args()
+        image_file = args["file"]
+        filename = f"{uuid4()}.csv"
+        image_file.save(filename)
+        print(filename)
--- llrws/config.py
+++ llrws/config.py
@@ -4,6 +4,8 @@
 Module to store Flask configurations.
 """
 
+import os
+
 
 class Config:
     """Flask app configuration class."""
@@ -11,3 +13,4 @@ class Config:
     DEBUG = True
     TESTING = False
     SERVER_NAME = "localhost:5000"
+    UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
