816e28dc37043032a3977eb6bc6c8b7a7b2f05ce
Ira Horecka <ira89@icloud.com>
2021-11-20 14:53:01 -0500

    update py interface with mave.r

--- llrws/api/llr.py
+++ llrws/api/llr.py
@@ -24,28 +24,27 @@ class LLR(Resource):
         session_id = uuid4()
 
         args = self.parse.parse_args()
-        # Save reference CSV file
+        # Validate and save reference CSV file
         ref_file = args["ref_file"]
         ref_filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], f"{session_id}-reference.csv")
         bool_file_saved, error_msg = save_CSV_fileobj_to_filepath(ref_file, ref_filepath)
         if bool_file_saved is False:
             return error_msg, 400
-        # Save score CSV file
+        # Validate and save score CSV file
         score_file = args["score_file"]
         score_filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], f"{session_id}-score.csv")
         bool_file_saved, error_msg = save_CSV_fileobj_to_filepath(score_file, score_filepath)
         if bool_file_saved is False:
             return error_msg, 400
 
-        # TODO: Validate CSV files
-
-        # Declare empty output_filepath, as output_filepath is otherwise declared in a try-block.
-        output_filepath = ""
+        # Declare output filepath for maveLLR-processed CSV file
+        output_filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], f"{session_id}-maveLLR.csv")
         try:
             # Execute R script ../rscripts/mave.r via Python wrapper scripts.execute_maveLLR_script
-            output_filepath = execute_maveLLR_script(
-                ref_filepath, score_filepath, current_app.config["UPLOAD_FOLDER"], unique_key=session_id
-            ).strip()
+            # stderr_out indicates either a stderr or stdout
+            bool_success, stderr_out = execute_maveLLR_script(ref_filepath, score_filepath, output_filepath)
+            if not bool_success:
+                return stderr_out, 400
             return send_file_for_download(output_filepath, "maveLLR.csv")
         finally:
             rm_files([ref_filepath, score_filepath, output_filepath])
--- llrws/rscripts/scripts.py
+++ llrws/rscripts/scripts.py
@@ -1,26 +1,51 @@
 import os
 import subprocess
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+RSCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def execute_maveLLR_script(reference_path, score_path, download_path, unique_key=None):
+def execute_maveLLR_script(reference_path, score_path, download_path):
     """Executes mave.r and writes unique output file to {}
 
     Args:
         reference_path (str): Path to reference CSV file
         score_path (str): Path to score CSV file
-        unique_key (str): Unique identifier
+        download_path (str): Path to maveLLR-processed CSV file
+
+    Returns:
+        (bool): Indicative of process success (True) or failure (False)
+        (str): Exported CSV filepath (utf-8) from mave Rscript
     """
-    mave_path = os.path.join(BASE_DIR, "mave.r")
-    call = [
+    mave_rscript_path = os.path.join(RSCRIPTS_DIR, "mave.r")
+    rscript_call = [
         "Rscript",
-        mave_path,
+        mave_rscript_path,
         f"--reference={reference_path}",
         f"--score={score_path}",
-        f"--downloadpath={download_path}",
-        f"--key={unique_key}",
+        f"--download={download_path}",
     ]
-    result = subprocess.Popen(call, stdout=subprocess.PIPE)
-    # Return exported filepath from R script's stdout
-    return result.stdout.read().decode("utf-8")
+    # At this point, nearly *all* data validation should be complete for reference_path
+    # and score_path.
+    return execute_subprocess(rscript_call)
+
+
+def execute_subprocess(script_call):
+    """Executes script call via subprocess.Popen. Returns True, stdout if execution
+    was successful. Returns False, stderr if execution failed.
+
+    Args:
+        script_call (str): Formatted script call for *NIX bash execution.
+
+    Returns:
+        (bool, str): Indicator of process success, stdout (if success) or stderror (if failure)
+    """
+    result = subprocess.Popen(script_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Wait for subprocess to complete prior to fetching return code.
+    result.wait()
+    return_code = result.returncode
+
+    # Non-zero exit code - indicate error and return stderr to caller.
+    if return_code != 0:
+        return False, result.stderr.read().decode("utf-8")
+    # Return success flag + stdout from Rscript execution.
+    return True, result.stdout.read().decode("utf-8")
