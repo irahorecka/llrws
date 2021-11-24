843c3f1b7094356a940d46e04ee2ec052c39e8c8
Ira Horecka <ira89@icloud.com>
2021-11-23 23:54:01 -0500

    update maveDataTable and maveDropZone - big improvement on dataTable

--- llrws/static/js/maveDataTable.js
+++ llrws/static/js/maveDataTable.js
@@ -1,66 +1,39 @@
 $(document).ready(function() {
-	// Declare common variables used in DataTables
-	var datatableLang = {
-		searchPlaceholder: "Search HGVS Pro"
-	}
-	var datatableLengthMenu = [
-		[50, 100, 250, -1],
-		[50, 100, 250, "All"]
-	]
 	// Initialzie with empty data table.
-	$("#data").DataTable({
-		language: datatableLang,
-		lengthMenu: datatableLengthMenu,
+	var table = $("#data").DataTable({
+		// Disable initial sorting
+		aaSorting: [],
+		columns: [
+			{data: "hgvs_pro", sortable: false},
+			{data: "score", searchable: false},
+			{data: "sd", searchable: false},
+			{data: "se", searchable: false},
+			{data: "llr",searchable: false},
+			{data: "llrCIlower", searchable: false},
+			{data: "llrCIupper", searchable: false},
+		],
+		language: {
+			searchPlaceholder: "Search HGVS Pro",
+		},
+		lengthMenu: [
+			[100, 250, 500, -1],
+			[100, 250, 500, "All"],
+		],
+		pageLength: 100,
+		scrollX: true,
+		scrollY: "500px",
+		scroller: true,
+		scrollCollapse: true,
 	});
 
 	// Get table content on button click.
 	$("#mave-button-submit").click(function() {
-		var table = $("#data").DataTable({
-			ajax: "/data",
-			columns: [{
-					data: "hgvs_pro"
-				},
-				{
-					data: "score",
-					searchable: false
-				},
-				{
-					data: "sd",
-					searchable: false
-				},
-				{
-					data: "se",
-					searchable: false
-				},
-				{
-					data: "llr",
-					searchable: false
-				},
-				{
-					data: "llrCIlower",
-					searchable: false
-				},
-				{
-					data: "llrCIupper",
-					searchable: false
-				},
-			],
-			// Destroy previous DataTable instance
-			destroy: true,
-			initComplete: function(settings, json) {
-				$("html, body").animate({
-					scrollTop: document.body.scrollHeight
-				}, "slow");
-			},
-			language: datatableLang,
-			lengthMenu: datatableLengthMenu,
-			pageLength: 50,
-			scrollX: true,
-			scrollY: "500px",
-			scroller: true,
-			scrollCollapse: true,
-			sorting: false,
+		$('#mave-button-submit').val('Loading...').prop('disabled', true);
+		table.ajax.url("/data").load(function() {
+			$('#mave-button-submit').val('Get MAVE').prop('disabled', false);
+			$("html, body").animate({
+				scrollTop: document.body.scrollHeight
+			}, "slow");
 		});
-		table.columns.adjust();
 	});
 });
