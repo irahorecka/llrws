5d705bbf0cf8c275d1e4a1f878fd36ec986138c1
Ira Horecka <ira89@icloud.com>
2021-11-24 11:38:08 -0500

    refine Dropzone border color

--- llrws/static/js/maveDropZone.js
+++ llrws/static/js/maveDropZone.js
@@ -1,7 +1,7 @@
 Dropzone.autoDiscover = false;
 $(".dropzone").dropzone({
     addRemoveLinks: true,
-    maxFiles: 1,
+    maxFiles: 2,
     removedfile: function(file) {
         var name = file.name;
         $.ajax({
@@ -11,5 +11,38 @@ $(".dropzone").dropzone({
         var _ref;
         return (_ref = file.previewElement) != null ? _ref
             .parentNode.removeChild(file.previewElement) : void 0;
+    },
+    init: function() {
+        // Get intance of Dropzone object
+        var maveCSVDropzone = Dropzone.forElement(".dropzone");
+        function invoke_blue_border() {
+            invoke_border_color('rgb(0, 135, 247)');
+        }
+        function invoke_red_border() {
+            invoke_border_color('rgb(220, 53, 69)');
+        }
+        function invoke_border_color(color) {
+            setTimeout(function(){
+                $('.dropzone').css('border', '2px dashed ' + color);
+            }, 200);
+        }
+
+        this.on("error", function(file, response) {
+            invoke_red_border();
+        });
+        this.on("removedfile", function(file, response) {
+            $(".dz-error-message span").each(function(){
+                // Check if no error messages are present
+                if (!$(this).text().trim().length) {
+                    invoke_blue_border();
+                } else {
+                    invoke_red_border();
+                }
+            });
+            // No more files in Dropzone instance
+            if (maveCSVDropzone.files.length == 0) {
+                invoke_blue_border();
+            }
+        });
     }
 });
