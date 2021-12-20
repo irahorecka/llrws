49a599f3fe768cfc5bd07d507397707eb93f5ef7
Ira Horecka <ira89@icloud.com>
2021-12-19 23:16:48 -0800

    I'm too lazy to write a commit message.

---
+++ test.py
@@ -0,0 +1,16 @@
+from io import StringIO
+
+import requests
+import pandas as pd
+
+
+files = {
+    "benchmark_file": open("CALM123_jointReference.csv", "rb"),
+    "score_file": open("CALM1_full_imputation_refined_mavedb.csv", "rb"),
+}
+
+response = requests.post("http://localhost:5000/api/", files=files)
+df = pd.read_csv(StringIO(response.content.decode("utf-8")))
+print(df["hgvs_pro"])
+df.to_csv("test.csv")
+print(df)
